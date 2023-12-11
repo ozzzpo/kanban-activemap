39124f2926313e30bf39e7d13aca01f2e50005a4
Warnoffline <hametshin543@gmail.com>
2023-12-11 17:30:26 +0300

    color-fix

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -6,14 +6,16 @@ const dotColors = {};
 export default function Column({ status }) {
   return (
     <div className='column'>
-      <div className='column_title'>
-        <div className='column_title_left'>
+      <div className='column_title' style={{boxShadow: `4px 4px 4px #${status.color}25`}}>
+        <div className='column_title_left' >
           <div
             className='circle'
             style={{ backgroundColor: `#${status.color}` }}
           ></div>
         </div>
-        <p>{status.name.charAt(0).toUpperCase() + status.name.slice(1)}</p>
+        <p
+        style={{color: `#${status.color}`}}
+        >{status.name.charAt(0).toUpperCase() + status.name.slice(1)}</p>
       </div>
       <Task></Task>
     </div>
