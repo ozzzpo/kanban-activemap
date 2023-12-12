7b92ba8fe4e791c78518ce6d217e44fac31b4938
ozzzpo <onehendrix04@gmail.com>
2023-12-12 06:32:24 +0300

    add photos upload to task

--- src/api/tasks.service.js
+++ src/api/tasks.service.js
@@ -13,6 +13,17 @@ const tasksService = {
     }
   },
 
+  deleteTasks: async (token, id) => {
+    try {
+      const response = await axios.delete(
+        `${API_BASE_URL}/tasks/${id}?token=${token}`
+      );
+      return response.data;
+    } catch (error) {
+      console.log(error.message);
+    }
+  },
+
   getFilteredTasks: async (token, assignedFioId, typeId = "1,2,3,4,5,6,7") => {
     try {
       const response = await axios.get(
@@ -38,6 +49,46 @@ const tasksService = {
   getTypes: async (token) => {
     try {
       const response = await axios.get(`${API_BASE_URL}/types?token=${token}`);
+      return response.data;
+    } catch (error) {
+      console.log(error.message);
+    }
+  },
+
+  uploadPhotos: async (token, file) => {
+    const formData = new FormData();
+    formData.append("file", file);
+    try {
+      const response = await axios.post(
+        `${API_BASE_URL}/files/upload/photos?token=${token}`,
+        formData,
+        {
+          headers: {
+            "Content-Type": " multipart/form-data",
+          },
+        }
+      );
+      return response.data;
+    } catch (error) {
+      console.log(error.message);
+    }
+  },
+  postPhotos: async (token, file, fileName, id) => {
+    const requestBody = {
+      attachments: [
+        {
+          file: file,
+          file_name: fileName,
+          file_type: "PHOTO",
+        },
+      ],
+      update_comments: "",
+    };
+    try {
+      const response = axios.patch(
+        `${API_BASE_URL}/tasks/${id}?apiVersion=2.1&token=${token}`,
+        requestBody
+      );
       return response.data;
     } catch (error) {
       console.log(error.message);
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,24 +1,49 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Modal from "./Modal";
+import tasksService from "../api/tasks.service";
 export default function Task({ task, isNew, token, color }) {
-  const [active, isActive] = useState(false)
+  const [active, isActive] = useState(false);
+  const [source, setSource] = useState("");
+  const handleLoad = (e) => {
+    tasksService.uploadPhotos(token, e.target.files[0]).then((data) => {
+      tasksService
+        .postPhotos(token, data.name, e.target.files[0].name, task.id)
+        .then((data) => console.log(data));
+    });
+  };
   return (
     <>
-    <div onClick={() => isActive(!active)} className='task-card' style={{ boxShadow: `4px 4px 4px #${color}25` }}>
-      <div className='main-task-info'>
-        <p className='task-title'>{task.title}</p>
-        <p>
-          <span>{isNew ? "Отдел: " : "Исполнитель: "}</span>
-          {isNew ? task.department_title : task.assigned_user_fio}
-        </p>
-        <p>
-          <span>Вид работы: </span>
-          {task.news_type_name}
+      <div
+        onClick={() => isActive(!active)}
+        className='task-card'
+        style={{ boxShadow: `4px 4px 4px #${color}25` }}
+      >
+        <div className='main-task-info'>
+          <p className='task-title'>{task.title}</p>
+          <p>
+            <span>{isNew ? "Отдел: " : "Исполнитель: "}</span>
+            {isNew ? task.department_title : task.assigned_user_fio}
+          </p>
+          <p>
+            <span>Вид работы: </span>
+            {task.news_type_name}
+          </p>
+        </div>
+        <p className='date'>
+          <span>Создано: </span>
+          {task.news_date}
         </p>
       </div>
-      <p className='date'>{task.deadline?.slice(0, 10)}</p>
-    </div>
-    {active && <Modal task={task} active={active} isActive={isActive} color={color} token={token}></Modal>}
+      {active && (
+        <Modal
+          task={task}
+          active={active}
+          isActive={isActive}
+          color={color}
+          token={token}
+          onLoad={handleLoad}
+        ></Modal>
+      )}
     </>
   );
 }
